04bb46a479f94e0ad999fd950c8e2966ce457c57
Aniketpn123 <aniketpn123@gmail.com>
2023-11-14 17:10:10 +0530

    changes

---
+++ form.js
@@ -0,0 +1,11 @@
+let name=document.getElementById('name-js');
+let subject= document.getElementById('subject-js');
+let email=document.getElementById('email-js');
+let message=document.getElementById('message-js');
+let info=[];
+
+function submit()
+{
+  
+info.push();
+}
